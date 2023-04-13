53efffc65c566152e626caa7eef1352d40c881a5
tomboyo <tomasimmons@gmail.com>
2023-04-13 01:26:43 -0400

    Rotate the character

--- Assets/Scripts/PlayerInputBehavior.cs
+++ Assets/Scripts/PlayerInputBehavior.cs
@@ -4,10 +4,16 @@ using UnityEngine.InputSystem;
 public class PlayerInputBehavior : MonoBehaviour
 {
 
+    [Header("Movement Parameters")]
+    public float moveSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+
     [Header("Component References")]
     public CharacterController characterController;
+    public Transform playerTransform;
 
-    private Vector3 move;
+    private Vector3 move = Vector3.zero;
+    private Vector3 turn;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -15,8 +21,22 @@ public class PlayerInputBehavior : MonoBehaviour
         move = new Vector3(vec.x, 0f, vec.y);
     }
 
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        var vec = context.ReadValue<Vector2>();
+        turn = new Vector3(vec.x, 0f, 0f);
+    }
+
     void Update()
     {
-        characterController.Move(move * Time.deltaTime);
+        var turnBy = Quaternion.FromToRotation(
+            Vector3.forward,
+            turn);
+        playerTransform.rotation = Quaternion.Slerp(
+            playerTransform.rotation,
+            playerTransform.rotation * turnBy,
+            Time.deltaTime * turnSpeed * turn.magnitude);
+
+        characterController.Move(playerTransform.rotation * (moveSpeed * Time.deltaTime * move));
     }
 }
